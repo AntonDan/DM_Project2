b5141a0e22c52b2080fae5be253ff869d617cf78
AntonDan <anton.dan@hotmail.com>
2018-05-22 22:35:16 +0300

    Initial commit

---
+++ project2.py
@@ -0,0 +1,130 @@
+from scipy.stats import mode
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.feature_extraction.text import CountVectorizer
+
+import pandas as pd
+from ast import literal_eval
+from random import randint
+from numpy import array, zeros, argmin, inf, equal, ndim
+from scipy.spatial.distance import cdist
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+def dtw(x, y, dist=euclidean_distances):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences.
+    :param array x: N1*M array
+    :param array y: N2*M array 
+    :param func dist: distance used as cost measure
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = dist(x, y)
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+def fastdtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences in a faster way.
+    Instead of iterating through each element and calculating each distance,
+    this uses the cdist function from scipy (https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html)
+    :param array x: N1*M array
+    :param array y: N2*M array
+    :param string or func dist: distance parameter for cdist. When string is given, cdist uses optimized functions for the distance metrics.
+    If a string is passed, the distance function can be 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski', 'yule'.
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    if ndim(x)==1:
+        x = x.reshape(-1,1)
+    if ndim(y)==1:
+        y = y.reshape(-1,1)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:]
+    D0[1:,1:] = cdist(x,y,dist)
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+def _traceback(D):
+    i, j = array(D.shape) - 2
+    p, q = [i], [j]
+    while ((i > 0) or (j > 0)):
+        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
+        if (tb == 0):
+            i -= 1
+            j -= 1
+        elif (tb == 1):
+            i -= 1
+        else: # (tb == 2):
+            j -= 1
+        p.insert(0, i)
+        q.insert(0, j)
+    return array(p), array(q)
+
+x = [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2], [4, 3], [2, 3], [1, 1], [2, 2], [0, 1], [2, 2], [4, 3], [2, 3], [1, 1]]
+y = [[1, 0], [1, 1], [1, 1], [2, 1], [4, 3], [4, 3], [2, 3], [3, 1], [1, 2], [1, 0], [2, 1], [4, 3]]
+
+dist, cost, acc, path = dtw(x, y, euclidean_distances)
+# trainSet = pd.read_csv(
+# 	'train_set2.csv', # replace with the correct path
+# 	converters={"Trajectory": literal_eval},
+# 	index_col='tripId'
+# )
+
+# trajectories = trainSet["Trajectory"]
+# journeyPatternIds = trainSet['journeyPatternId']
+# samples = []
+# for i in range(0, len(trajectories.keys())):
+#     index = trajectories.keys()[i]                                # Get a random applicable index on the dataset
+#     samples.append([])
+#     for t, lon, lat in trajectories[index]:                         # Fill them with the desired trip info
+#         samples[i].append([lon, lat])
+#     samples[i] = [samples[i]]
+# test=samples[0]
+# samples=samples[1:3]
+# samples=np.array(samples)
+# print samples
+# samples.shape
+
+# dist, cost, acc, path = dtw(samples[0], test[0], euclidean_distances)
+# neigh = NearestNeighbors(n_neighbors=1, metric=dtw)
+# neigh.fit(samples)
+# print(neigh.kneighbors(test)) 
+# vizualize
+from matplotlib import pyplot as plt
+plt.imshow(cost.T, origin='lower', cmap=plt.cm.Reds, interpolation='nearest')
+plt.plot(path[0], path[1], '-o') # relation
+plt.xticks(range(len(x)), x)
+plt.yticks(range(len(y)), y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.axis('tight')
+plt.title('Minimum distance: {}'.format(dist))
+plt.show()
