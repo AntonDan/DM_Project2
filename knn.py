47da5f75cea25602428090f8c85dde95cd85f7e0
Theo <theo.cj@gmail.com>
2018-05-26 01:29:17 +0300

    finished a1 question

--- knn.py
+++ knn.py
@@ -3,7 +3,9 @@ import itertools
 import operator
 
 from numpy import array, zeros, argmin, inf, equal, ndim
+from scipy.spatial.distance import cdist
 
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
@@ -33,49 +35,38 @@ class KNeighborsClassifier(BaseEstimator, ClassifierMixin):
 
         return self
 
-    def predict(self, X):
+    def predict(self, X, dist):
         # Check if fit had been called
         check_is_fitted(self, ['X_', 'y_'])
 
         # Input validation
-        X = check_array(X, accept_sparse='csr')
+        # X = check_array(X, accept_sparse='csr')
 
         predicted_labels = list()
 
-        # We create a similarity matrix between our train set and our test set
-        # This pairwise function operates on the train and the test set, to create a 2D matrix of cosine similarity between them
-        similarity_matrix = cosine_similarity(self.X_, X)
+        for qitem in X:
+            neighbors_distance = list()
+            for idx, fitem in enumerate(self.X_):
+                neighbors_distance.append( (idx, fastdtw(fitem, qitem, dist)) )
 
-        for idx, document in enumerate(X):
-            neighbors = self.get_neighbors(similarity_matrix, idx)
-            predicted_labels.append(self.majority_voting(neighbors))
+            sorted_neighbors_distance = sorted(neighbors_distance, key=lambda neighbors_distance: neighbors_distance[1]) #, reverse = True)
+            predicted_labels.append(self.majority_voting(list(itertools.islice(sorted_neighbors_distance, self.n_neighbors))))
 
         return np.array(predicted_labels)
 
-    # Getting neighbors by finding the most similar documents through the similarity matrix
-    def get_neighbors(self, similarity_matrix, document_index):
-        potential_neighbors = list()
-        for idx, document in enumerate(self.X_):
-            potential_neighbors.append(similarity_matrix[idx, document_index])
-
-        enumlist = enumerate(potential_neighbors)
-        pot_nlist = sorted(enumlist, key=lambda enumlist: enumlist[1], reverse = True)
-        return list(itertools.islice(pot_nlist, self.n_neighbors))
-
-    # Weighted majority voting. Votes are cast by using the similarity to the test document, so that predictions are more accurate
     def majority_voting(self, neighbors):
         majority_dict = dict()
 
-        for neighbor_index, similarity in neighbors:
+        for neighbor_index, distance in neighbors:
             if self.y_[neighbor_index] in majority_dict:
-                majority_dict[self.y_[neighbor_index]] += similarity
+                majority_dict[self.y_[neighbor_index]] += distance
             else:
-                majority_dict[self.y_[neighbor_index]] = similarity
+                majority_dict[self.y_[neighbor_index]] = distance
 
-        return max(majority_dict.items(), key=operator.itemgetter(1))[0]
+        return min(majority_dict.items(), key=operator.itemgetter(1))[0]
 
     # Needs to return the k best neighbors (we need those with the least distance)
-    def kneighbors(self, X, n_neighbors=5):
+    def kneighbors(self, X, dist, n_neighbors=5):
         # Check if fit had been called
         check_is_fitted(self, ['X_', 'y_'])
 
@@ -83,14 +74,14 @@ class KNeighborsClassifier(BaseEstimator, ClassifierMixin):
         # Input validation
         # X = check_array(X, accept_sparse='csr')
 
-        dist = DistanceMetric.get_metric('haversine')
+        # dist = DistanceMetric.get_metric('haversine')
 
         k_neighbors = list()
 
         for qitem in X:
             neighbors_distance = list()
             for idx, fitem in enumerate(self.X_):
-                neighbors_distance.append( (idx, dtw(fitem, qitem, dist)) )
+                neighbors_distance.append( (idx, fastdtw(fitem, qitem, dist)) )
 
             sorted_neighbors_distance = sorted(neighbors_distance, key=lambda neighbors_distance: neighbors_distance[1]) #, reverse = True)
             k_neighbors.append(list(itertools.islice(sorted_neighbors_distance, n_neighbors)))
@@ -99,14 +90,13 @@ class KNeighborsClassifier(BaseEstimator, ClassifierMixin):
         for item in k_neighbors:
             temp = list()
             for neighbor in item:
-                temp.append( (self.y_[neighbor[0]], neighbor[1], self.X_[neighbor[0]]) )
+                temp.append( (self.y_[neighbor[0]], neighbor[1][0], self.X_[neighbor[0]]) )
             if len(temp) > 0:
                 k_neighbors_actual.append(temp)
 
         return k_neighbors_actual
 
-# DTW computation
-def dtw(x, y, dist):
+def dtw(x, y, dist=euclidean_distances):
     """
     Computes Dynamic Time Warping (DTW) of two sequences.
     :param array x: N1*M array
@@ -120,10 +110,8 @@ def dtw(x, y, dist):
     D0 = zeros((r + 1, c + 1))
     D0[0, 1:] = inf
     D0[1:, 0] = inf
-    D1 = D0[1:, 1:] # view
-    for i in range(r):
-        for j in range(c):
-            D1[i, j] = dist.pairwise([x[i]], [y[j]])
+    D1 = dist(x, y)
+    print(D1)
     C = D1.copy()
     for i in range(r):
         for j in range(c):
@@ -134,7 +122,42 @@ def dtw(x, y, dist):
         path = range(len(x)), zeros(len(x))
     else:
         path = _traceback(D0)
-    return D1[-1, -1] / sum(D1.shape)#, C, D1, path
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+def fastdtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences in a faster way.
+    Instead of iterating through each element and calculating each distance,
+    this uses the cdist function from scipy (https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html)
+    :param array x: N1*M array
+    :param array y: N2*M array
+    :param string or func dist: distance parameter for cdist. When string is given, cdist uses optimized functions for the distance metrics.
+    If a string is passed, the distance function can be 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski', 'yule'.
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    if ndim(x)==1:
+        x = x.reshape(-1,1)
+    if ndim(y)==1:
+        y = y.reshape(-1,1)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:]
+    D0[1:,1:] = cdist(x,y,dist)
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
 
 def _traceback(D):
     i, j = array(D.shape) - 2
--- knntest.py
+++ knntest.py
@@ -12,8 +12,24 @@ from gmplot import gmplot
 
 from knn import KNeighborsClassifier
 
+import math
+
+def haversine(X, Y):
+    lat1, lon1 = X 
+    lat2, lon2 = Y
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
 trainSet = pd.read_csv(
-	'train_set2.csv', # replace with the correct path
+	'../train_set.csv', # replace with the correct path
 	converters={"Trajectory": literal_eval},
 	index_col='tripId'
 )
@@ -22,11 +38,18 @@ trajectories = trainSet["Trajectory"]
 journeyPatternIds = trainSet['journeyPatternId']
 
 testSet = pd.read_csv(
-	'test_set_a1.csv', # replace with the correct path
+	'../test_set_a1.csv', # replace with the correct path
     sep='\t',
     converters={"Trajectory": literal_eval}
 )
 testSet = testSet["Trajectory"]
+
+testSet2 = pd.read_csv(
+	'../test_set_a2.csv', # replace with the correct path
+    sep='\t',
+    converters={"Trajectory": literal_eval}
+)
+testSet2 = testSet2["Trajectory"]
 
 samples = []
 for i in range(0, len(trajectories.keys())):
@@ -42,45 +65,61 @@ for i in range(0, len(journeyPatternIds.keys())):
 
 test = []
 for i in range(0, len(testSet.keys())):
-    index = testSet.keys()[i]                                # Get a random applicable index on the dataset
+    index = testSet.keys()[i]
     test.append([])
-    for t, lon, lat in testSet[index]:                         # Fill them with the desired trip info
+    for t, lon, lat in testSet[index]:
         test[i].append([lat, lon])
+
+test2 = []
+for i in range(0, len(testSet2.keys())):
+    index = testSet2.keys()[i]
+    test2.append([])
+    for t, lon, lat in testSet2[index]:
+        test2[i].append([lat, lon])
 
 samples = samples[:10]
 labels = labels[:10]
 test = test[:2]
+# test2 = test2[:1]
 
-neigh = KNeighborsClassifier(n_neighbors=1)
+neigh = KNeighborsClassifier(n_neighbors=5)
 neigh.fit(samples, labels)
 
-result = neigh.kneighbors(test, 2)
-print(result)
-print('='*60)
+# result = neigh.kneighbors(test, haversine, 5) # a1 question
+result = neigh.predict(test2, haversine)    # 3rd question
 
-for answer in result:
-    for (journey_id, dtw_distance, trajectory) in answer:
+# 3rd question
+dic = {
+    "Test_Trip_ID" : range(len(test2)),
+    "Predicted_JourneyPatternID" : result
+}
+out_df = pd.DataFrame(dic, columns=['Test_Trip_ID', 'Predicted_JourneyPatternID'])
+out_df.to_csv("testSet_JourneyPatternIDs.csv", sep='\t', index=False)
 
-        print ("journey_id: ", journey_id, " dtw_distance: ", dtw_distance)
-        # print (trajectory)
+# a1 question
+# for i, answer in enumerate(result):
+#     gmap = gmplot.GoogleMapPlotter(53.383015, -6.237581, 12)
 
+#     longitudes = list()
+#     latitudes = list()
+#     for lat, lon in test[i]:
+#         longitudes.append(lon)
+#         latitudes.append(lat)
 
-# gmap = gmplot.GoogleMapPlotter(53.383015, -6.237581, 12)
+#     gmap.plot(latitudes, longitudes, "red", edge_width=2)		# Plot it on the map
+#     gmap.draw("Test Trip " + str(i+1) + ".html")
 
-# longitudes = list()
-# latitudes = list()
-# for lat, lon in test[0]:
-#     longitudes.append(lon)
-#     latitudes.append(lat)
+#     for j, (journey_id, dtw_distance, trajectory) in enumerate(answer):
 
-# gmap.plot(latitudes, longitudes, "black", edge_width=2)		# Plot it on the map
+#         # print ("journey_id: ", journey_id, " dtw_distance: ", dtw_distance)
+#         # print (trajectory)
 
-# longitudes = list()
-# latitudes = list()
-# for lat, lon in result[0]:
-#     longitudes.append(lon)
-#     latitudes.append(lat)
+#         longitudes = list()
+#         latitudes = list()
+#         for lat, lon in trajectory:
+#             longitudes.append(lon)
+#             latitudes.append(lat)
 
-# gmap.plot(latitudes, longitudes, "red", edge_width=2)		# Plot it on the map
-
-# gmap.draw("eyetest.html")
+#         gmap.plot(latitudes, longitudes, "red", edge_width=2)		# Plot it on the map
+#         print("Test Trip " + str(i+1) + " Neighbor " + str(j+1) + " j_id: " + str(journey_id) + " dtw_dist: " + str(dtw_distance))
+#         gmap.draw("TT " + str(i+1) + " Neighbor " + str(j+1) + ".html")
