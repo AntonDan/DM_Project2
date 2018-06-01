096aab8ed7c576a7aa9dfb6d3eabb90e7abd6033
AntonDan <anton.dan@hotmail.com>
2018-06-01 21:55:21 +0300

    Cleaning up

--- project_a2.py
+++ project_a2.py
@@ -15,57 +15,17 @@ from gmplot import gmplot
 from timeit import default_timer as timer
 
 import math
+from haversine import haversine
 
-def haversine(X, Y):
-    lat1, lon1 = X 
-    lat2, lon2 = Y
-    radius = 6371 # km
-
-    dlat = math.radians(lat2-lat1)
-    dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = radius * c
-
-    return d 
-
-def fastdtw(x, y, dist):
-    """
-    Computes Dynamic Time Warping (DTW) of two sequences in a faster way.
-    Instead of iterating through each element and calculating each distance,
-    this uses the cdist function from scipy (https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html)
-    :param array x: N1*M array
-    :param array y: N2*M array
-    :param string or func dist: distance parameter for cdist. When string is given, cdist uses optimized functions for the distance metrics.
-    If a string is passed, the distance function can be 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski', 'yule'.
-    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
-    """
-    assert len(x)
-    assert len(y)
-    if ndim(x)==1:
-        x = x.reshape(-1,1)
-    if ndim(y)==1:
-        y = y.reshape(-1,1)
-    r, c = len(x), len(y)
-    D0 = zeros((r + 1, c + 1))
-    D0[0, 1:] = inf
-    D0[1:, 0] = inf
-    D1 = D0[1:, 1:]
-    D0[1:,1:] = cdist(x,y,dist)
-    C = D1.copy()
-    for i in range(r):
-        for j in range(c):
-            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
-    if len(x)==1:
-        path = zeros(len(y)), range(len(y))
-    elif len(y) == 1:
-        path = range(len(x)), zeros(len(x))
-    else:
-        path = _traceback(D0)
-        return D1[-1, -1] / sum(D1.shape), C, D1, path
-
-def lcs_length(a, b):
+# LCSS function
+# Input: a[1...m], b[1...n]
+# Returns: the longest common subsequence and its length
+# Creates a 2 dimensional matrix table[1...m,1...n] and initializes it
+# for every coordinate in a then for every coordinate in j 
+# if those coordinates represent points within 200 meteres
+# update the respective table element using the previous (diagonally) element info
+# else just pass on the top or left element based on which has the largest length   
+def lcss(a, b):
     table = [[([],0)] * (len(b) + 1) for _ in range(len(a) + 1)]
     for i, ca in enumerate(a, 1):
         for j, cb in enumerate(b, 1):
@@ -82,27 +42,9 @@ def lcs_length(a, b):
     path, length = table[-1][-1]
     return path, length
 
-def _traceback(D):
-    i, j = array(D.shape) - 2
-    p, q = [i], [j]
-    while ((i > 0) or (j > 0)):
-        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
-        if (tb == 0):
-            i -= 1
-            j -= 1
-        elif (tb == 1):
-            i -= 1
-        else: # (tb == 2):
-            j -= 1
-        p.insert(0, i)
-        q.insert(0, j)
-    return array(p), array(q)
-
-
-
 # MAIN
 testSet = pd.read_csv(
-	'../test_set_a2.csv',
+	'../test_set_a2.csv', # replace with the correct path
 	converters={"Trajectory": literal_eval},
     delimiter='\t'    
 )
@@ -124,7 +66,6 @@ for i in range(0, len(trajectories.keys())):
     samples.append([])
     for t, lon, lat in trajectories[index]:                         # Fill them with the desired trip info
         samples[i].append([lat, lon])
-# samples=samples[1:100]
 
 test = []
 for i in range(0, len(test_trajectories.keys())):
@@ -141,9 +82,8 @@ for t in range(0, len(test)):
     start_time = timer()
     print ("Test " , t)
     for i in range(0,len(samples)):
-        path, length = lcs_length(samples[i], test[t])
+        path, length = lcss(samples[i], test[t])
         if (length > minimum):
-    #     dist, cost, acc, path = fastdtw(samples[i], test[0], haversine)
             if (len(top) < 5):
                 top += [(trajectories.keys()[i], path, length)]
             else:
